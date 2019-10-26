94b0c1e69627ec46599bdec2073b1dcb31cd0623
Andrae Delisser <delisserad@gmail.com>
2019-10-26 19:33:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-end
+     
+     return 
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -130,5 +131,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages]
+  
 end
